f423ba136bb8933d5195d1d70b32c3d3e7e7caed
Moritz Hoffmann <clonker@gmail.com>
2021-05-11 14:26:15 +0200

    latest

--- trunk/_static/documentation_options.js
+++ trunk/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.7+0.g49e799a.dirty',
+    VERSION: '0.3.1+23.ga9f1138.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
