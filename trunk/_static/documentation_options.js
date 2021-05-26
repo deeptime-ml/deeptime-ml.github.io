dbd8504ee5374602482a42019673a395075950ac
Moritz Hoffmann <clonker@gmail.com>
2021-05-26 14:05:59 +0200

    trunk update

--- trunk/_static/documentation_options.js
+++ trunk/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.7+0.g49e799a.dirty',
+    VERSION: '0.2.7+5.gb22c33f0.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
