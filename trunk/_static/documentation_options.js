0a194643caba032d19d269b3bd68fa7d67558ece
Moritz Hoffmann <clonker@gmail.com>
2021-12-17 14:36:23 +0100

    docs update

--- trunk/_static/documentation_options.js
+++ trunk/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0',
+    VERSION: '0.3.1+7.g45c8b878.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
