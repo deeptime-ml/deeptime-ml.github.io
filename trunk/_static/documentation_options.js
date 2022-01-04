3a10753ddd543cc71ea857904c6e3c16041ea97c
Moritz Hoffmann <clonker@gmail.com>
2022-01-04 15:25:41 +0100

    tram

--- trunk/_static/documentation_options.js
+++ trunk/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1+7.g45c8b878.dirty',
+    VERSION: '0.3.1+10.g65eecc5c.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
