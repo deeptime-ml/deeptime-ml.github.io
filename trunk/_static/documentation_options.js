0f59fd9b3b59406b1adea8474201ddd64cee9165
MaaikeG <maaikegalama@outlook.com>
2022-01-13 13:20:34 +0100

    updated trunk TRAM docs

--- trunk/_static/documentation_options.js
+++ trunk/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1',
+    VERSION: '0.3.1+23.ga9f1138.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
